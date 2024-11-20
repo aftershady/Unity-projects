198a8c0511eb420d973cac9857ee769d376d0b12
Jonathan <shady14@hotmail.fr>
2024-11-20 10:13:04 +0100

    add reticule in the center of the vision and change the shooting script

--- project test sandbox/Assets/scripts/shooting.cs
+++ project test sandbox/Assets/scripts/shooting.cs
@@ -2,31 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// The Shooting class detects a left mouse click, casts a ray from the camera,
-// and destroys an enemy GameObject if it is hit.
 public class Shooting : MonoBehaviour
 {
-    private void Update()
+    //create reticule variable ans copy to main camera
+    public RectTransform reticule;
+    private Camera mainCamera;
+
+    void Start()
     {
-        // Detects a left mouse button click
+        //copy main camera
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        // check if mouse button 0 is pressed
         if (Input.GetMouseButtonDown(0))
         {
-            // Creates a ray from the camera to the mouse position
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            /*point a raycast to the center of the reticule position
+            Ray ray : create a Raycast
+            mainCamera.ScreenPointToRay(reticule.position) : define the reticule position as raycast point*/
+            Ray ray = mainCamera.ScreenPointToRay(reticule.position);
+            //create a hit variable to store raycast output
             RaycastHit hit;
 
-            // Draws the ray in the Scene view for visualization, extending 50 units in red
-            Debug.DrawRay(ray.origin, ray.direction * 50, Color.red);
-
-            // Checks if the ray hits any object within a 50-unit range
+            // if Physics.raycast with a ray starting to the center of the reticule touche an object (store
+            // out in hit, and if this object is in 50 units of distance)
             if (Physics.Raycast(ray, out hit, 50f))
             {
-                // If the object hit has the tag "enemy," it will be destroyed
+                // if object have the enemis tag
                 if (hit.transform.gameObject.tag == "enemis")
                 {
+                    //destroy object
                     Destroy(hit.transform.gameObject);
                 }
             }
+
+            // for see the raycast in the scene view
+            Debug.DrawRay(ray.origin, ray.direction * 50, Color.red, 1f);
         }
     }
 }
