3f1cb6f9680d1a37462671424f34192e4732e41b
Jonathan <shady14@hotmail.fr>
2024-12-10 04:38:54 +0100

    fix enemy position, sort hierarchy & delete debug test in script

--- 2d learning project/Assets/scripts/enemyPatrol.cs
+++ 2d learning project/Assets/scripts/enemyPatrol.cs
@@ -15,13 +15,11 @@ public class enemyPatrol : MonoBehaviour
     // Update is called once per frame
 void Update()
 {
-    Debug.Log("Current Target: " + target.name + " | Distance: " + Vector3.Distance(transform.position, target.position));
 
     transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
     if (Vector3.Distance(transform.position, target.position) < 0.3f)
     {
-        Debug.Log("Switching to next waypoint");
         destPoint = (destPoint + 1) % wayPoints.Length;
         target = wayPoints[destPoint];
     }
