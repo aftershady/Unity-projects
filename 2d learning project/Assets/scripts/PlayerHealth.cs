c84053dfbed8e7387123563e94359815252dbc40
Jonathan <shady14@hotmail.fr>
2025-01-04 11:08:09 +0100

    add damage to player if hit an enemy and invicibility time after hit

--- 2d learning project/Assets/scripts/PlayerHealth.cs
+++ 2d learning project/Assets/scripts/PlayerHealth.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
+    /*******************************************************************************/
+    //SET HEALTH
     //this is the place where you set the health bar at the start
     public int maxHealth = 100;
     //health who will be updated
     public int currentHealth;
     //adding healthbar component
+    /*******************************************************************************/
+    // IF PLAYER IS HIT
+    //sprite renderer of player
+    public SpriteRenderer graphics;
+    //active when the player is hit
+    public bool isInvicible = false;
+    //delay beteween alpha 0.0 and alpha 1.0 of player
+    public float invincibilityFlashDelay = 1f;
+    //duration of invicibility
+    public float invicibilityAfterHit = 1.5f;
+    /*******************************************************************************/
+    //PHYSICS HEALTH BAR
     public HealthBar healthBar;
     //reference to player
     public GameObject Player;
+    /*******************************************************************************/
 
 
     void Start()
@@ -42,9 +58,35 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        //update the variable of health
-        currentHealth -= damage;
-        //update the fill and the gradient
-        healthBar.SetHealth(currentHealth);
+        //take damade only if isInvincible = false
+        if(!isInvicible)
+        {
+            //update the variable of health
+            currentHealth -= damage;
+            //update the fill and the gradient
+            healthBar.SetHealth(currentHealth);
+            isInvicible = true;
+            StartCoroutine(InvicibilityFlash());
+            StartCoroutine(handleInvincibilityDelay());
+        }
+    }
+
+    public IEnumerator InvicibilityFlash()
+    {
+        //while variable isInvicible is true, the graphics alpha of character altern from 0 to 1 every xf time
+        while (isInvicible)
+        {
+            graphics.color = new Color(1F, 1F, 1F, 0F);
+            yield return new WaitForSeconds(invincibilityFlashDelay);
+            graphics.color = new Color(1F, 1F, 1F, 1F);
+            yield return new WaitForSeconds(invincibilityFlashDelay);
+        }
+    }
+
+    public IEnumerator handleInvincibilityDelay()
+    {
+        //handle the duration of invicibility
+        yield return new WaitForSeconds(invicibilityAfterHit);
+        isInvicible = false;
     }
 }
--- 2d learning project/Assets/scripts/enemyPatrol.cs
+++ 2d learning project/Assets/scripts/enemyPatrol.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class enemyPatrol : MonoBehaviour
 {
+    /*******************************************************************************/
+    //MOVEMENT
+    /*******************************************************************************/
     //speed of enemy
     public float speed;
     // array of the class transform to set waypoints
     public Transform[] wayPoints;
     // point targeted by the enemy movement
-    public SpriteRenderer graphics;
-    // store SpriteRenderer in a variable for flip
     private Transform target;
     // index of the waypoints array
     private int destPoint = 0;
+    /*******************************************************************************/
+    //GIVE DAMAGE
+    /*******************************************************************************/
+    public int damageOnCollision = 20;
+    /*******************************************************************************/
+    //ANIMATION
+    /*******************************************************************************/
+    public SpriteRenderer graphics;
+    // store SpriteRenderer in a variable for flip
 
     void Start()
     {
@@ -20,22 +30,35 @@ public class enemyPatrol : MonoBehaviour
     }
 
     // Update is called once per frame
-void Update()
-{
-    //position of enemy = move with position of enemy has init, target.position has target, speed * fps for the delay
-    transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-
-    // if the distance between the enemy and the actual targeted waypoint is < of 0.3 (to avoid error)
-    if (Vector3.Distance(transform.position, target.position) < 0.3f)
+    void Update()
     {
-        // the index of target is calculated with modulo
-        // if the despoint is the index 0 of waypoints destpoint = 0 + 1 / number of index (ex 2)
-        // 0 + 1 / 2 = 1 go to the waypoint 1
-        // 1 + 1 / 2 = 0 got to the waypoint 0
-        destPoint = (destPoint + 1) % wayPoints.Length;
-        target = wayPoints[destPoint];
-        //flip the 2d model
-        graphics.flipX = !graphics.flipX;
+        Vector3 dir = target.position - transform.position;
+        //position of enemy = move with position of enemy has init, target.position has target, speed * fps for the delay
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        // if the distance between the enemy and the actual targeted waypoint is < of 0.3 (to avoid error)
+        if (Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            // the index of target is calculated with modulo
+            // if the despoint is the index 0 of waypoints destpoint = 0 + 1 / number of index (ex 2)
+            // 0 + 1 / 2 = 1 go to the waypoint 1
+            // 1 + 1 / 2 = 0 got to the waypoint 0
+            destPoint = (destPoint + 1) % wayPoints.Length;
+            target = wayPoints[destPoint];
+            //flip the 2d model
+            graphics.flipX = !graphics.flipX;
+        }
     }
-}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            // PlayerHealth playerHealth = create a new variable of type Playerhealth
+            // = variable colision.//directional coordinates//.//get an atached script of type playerhealth//
+            PlayerHealth playerHealth = collision.transform.GetComponent<PlayerHealth>();
+            // give damage to player
+            playerHealth.TakeDamage(damageOnCollision);
+        }
+    }
 }
