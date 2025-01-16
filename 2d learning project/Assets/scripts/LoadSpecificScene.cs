5dfbfe9a25ffd7ae620920bd7d6ce8eba670ade9
Jonathan <shady14@hotmail.fr>
2025-01-16 08:24:32 +0100

    add public variable to change scene script, change how work the falldownevent, add GameManager and a player spawn to handle level continuity

--- 2d learning project/Assets/scripts/LoadSpecificScene.cs
+++ 2d learning project/Assets/scripts/LoadSpecificScene.cs
@@ -3,12 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class LoadSpecificScene : MonoBehaviour
 {
+    public string sceneName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //if the player tag enter in coin
         if(collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Level 02");
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
--- 2d learning project/Assets/scripts/fallDownEvent.cs
+++ 2d learning project/Assets/scripts/fallDownEvent.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 
 public class fallDownEvent : MonoBehaviour
 {
-    public GameObject Player;
     private void OnTriggerEnter2D(Collider2D colision)
     {
-        if(colision.CompareTag("Player"))
+        if (colision.CompareTag("Player"))
         {
-            //return to starting position if falling
-            Player.transform.position = new Vector3(-11.162f, 1.5f, 0f);
+            // Retourner le joueur à la position de départ
+            colision.transform.position = new Vector3(-11.162f, 1.5f, 0f);
         }
     }
 }
