9cf634d5e35f96e64a2940f9c33a408ebd4d96f7
Jonathan <shady14@hotmail.fr>
2025-02-25 18:59:27 +0100

    add first part of main menu

---
+++ 2d learning project/Assets/scripts/SettingsMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+
+    public void Start()
+    {
+        //catch resolution of user
+        resolutions = Screen.resolutions;
+        //clear defaut dropdown options
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+}
