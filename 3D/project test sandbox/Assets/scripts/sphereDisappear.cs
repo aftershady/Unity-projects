34fed6a353cb9bc0f24bd87ea567e6f43fb2c1b5
Jonathan <shady14@hotmail.fr>
2025-04-21 16:52:47 +0200

    Reorganisation of folders

---
+++ 3D/project test sandbox/Assets/scripts/sphereDisappear.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The Test class toggles the visibility of a specified GameObject (maSphere)
+// based on whether the "R" key is pressed.
+public class SphereDisappear : MonoBehaviour
+{
+    // Reference to a GameObject (maSphere) to be shown or hidden
+    public GameObject maSphere;
+
+    void Update()
+    {
+        // Checks if the "R" key is being pressed
+        if (Input.GetKey(KeyCode.R))
+        {
+            // Activates maSphere, making it visible in the scene
+            maSphere.SetActive(false);
+        }
+        else
+        {
+            // Deactivates maSphere, making it invisible in the scene
+            maSphere.SetActive(true);
+        }
+    }
+}
