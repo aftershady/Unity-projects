8510004c468714f7a12250689ae3324c4b9828eb
Jonathan <shady14@hotmail.fr>
2025-04-29 13:58:01 +0200

    add chest at the end of the game

--- 2D/King's Adventure/Assets/scripts/Chest.cs
+++ 2D/King's Adventure/Assets/scripts/Chest.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    public GameObject openText;
+    public AudioSource audioSource;
+    public AudioClip openChestSound;
+
+
+    private void Awake()
     {
-        
+        openText = GameObject.FindGameObjectWithTag("ChestText");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            foreach (Transform child in openText.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                animator.SetTrigger("OpenChest");
+                StartCoroutine(PlayOpenChestSoundRepeatedly());
+                openText.SetActive(false);
+            }
+        }
     }
+
+    private IEnumerator PlayOpenChestSoundRepeatedly()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Inventory.instance.AddCoins(1);
+            audioSource.PlayOneShot(openChestSound);
+            yield return new WaitForSeconds(0.1F);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        foreach (Transform child in openText.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+    }
+
 }
