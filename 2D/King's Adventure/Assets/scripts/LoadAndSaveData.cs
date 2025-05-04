42a55c85c18f5675bccbb5403ab908279033af14
Jonathan <shady14@hotmail.fr>
2025-05-04 18:09:26 +0200

    start to implement save system

--- 2D/King's Adventure/Assets/scripts/LoadAndSaveData.cs
+++ 2D/King's Adventure/Assets/scripts/LoadAndSaveData.cs
@@ -7,7 +7,7 @@ public class LoadAndSaveData : MonoBehaviour
 {
     public static LoadAndSaveData instance;
 
-
+    private bool isGameLoaded = false;
     private void Awake()
     {
         if(instance != null)
@@ -17,21 +17,46 @@ public class LoadAndSaveData : MonoBehaviour
         instance = this;
     }
 
+    private void Start()
+    {
+        Time.timeScale = 1;
+        isGameLoaded = PlayerPrefs.GetInt("isGameLoaded", 0) == 1;
+        if(isGameLoaded == true)
+        {
+            LoadData();
+            PlayerPrefs.SetInt("isGameLoaded", 0);
+        }
+
+    }
+
+    public void continueGame()
+    {
+        PlayerPrefs.SetInt("isGameLoaded", 1);
+        int levelIndex = PlayerPrefs.GetInt("Level");
+        if (levelIndex == 0)
+        {
+            levelIndex = 1;
+        }
+        SceneManager.LoadScene(levelIndex);
+    }
+
     public void SaveData()
     {
         PlayerPrefs.SetInt("PlayerHealth", PlayerHealth.instance.currentHealth);
         PlayerPrefs.SetInt("CoinsCount", Inventory.instance.coinsCount);
-        if (SceneManager.GetActiveScene().name == "Level 01")
-        {
-            PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex);
-        }
-        else if (SceneManager.GetActiveScene().name == "Level 02")
+        if (SceneManager.GetActiveScene().name == "Level 02")
         {
             PlayerPrefs.SetInt("Level", 3); // Assuming "Level 02-2" corresponds to build index 3
         }
         else if (SceneManager.GetActiveScene().name == "Level 03")
         {
+            
             PlayerPrefs.SetInt("Level", 5); // Assuming "Level 03" corresponds to build index 5
+        }
+        else
+        {
+            Debug.Log("LoadAndSaveData Start");
+            PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex);
         }
         PlayerPrefs.SetFloat("PlayerPositionX", PlayerMovement.instance.transform.position.x);
         PlayerPrefs.SetFloat("PlayerPositionY", PlayerMovement.instance.transform.position.y);
@@ -43,8 +68,6 @@ public class LoadAndSaveData : MonoBehaviour
     {
         PlayerHealth.instance.currentHealth = PlayerPrefs.GetInt("PlayerHealth");
         Inventory.instance.coinsCount = PlayerPrefs.GetInt("CoinsCount");
-        int levelIndex = PlayerPrefs.GetInt("Level");
-        SceneManager.LoadScene(levelIndex);
         float playerPosX = PlayerPrefs.GetFloat("PlayerPositionX");
         float playerPosY = PlayerPrefs.GetFloat("PlayerPositionY");
         PlayerMovement.instance.transform.position = new Vector3(playerPosX, playerPosY, 0);
