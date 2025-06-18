6a3530d26372dbfcac1a5460b28cea06b8ee12d2
Aftershady <shady14@hotmail.fr>
2025-06-18 05:58:04 +0200

    start to set player movements

--- 3D/Windows/RPGguy/Assets/scripts/CharacterMotor.cs
+++ 3D/Windows/RPGguy/Assets/scripts/CharacterMotor.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CharacterMotor : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //speed
+    public float walkSpeeed;
+    public float runSpeed;
+    public float turnSpeed;
+
+    //inputs
+    public string inputFront;
+    public string inputBack;
+    public string inputLeft;
+    public string inputRight;
+
+    public Vector3 jumpspeed;
+    CapsuleCollider playerCollider;
+
+
     void Start()
     {
-        
+        playerCollider = gameObject.GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(inputFront))
+        {
+            transform.Translate(0, 0, walkSpeeed * Time.deltaTime);
+        }
         
+        if (Input.GetKey(inputBack))
+        {
+            transform.Translate(0, 0, -(walkSpeeed / 2) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(inputLeft))
+        {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+        
+        if (Input.GetKey(inputRight))
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
     }
 }
