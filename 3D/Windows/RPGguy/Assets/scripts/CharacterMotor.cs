02af1adc6f2f7792c0197f0dcac33a09f261f59b
Aftershady <shady14@hotmail.fr>
2025-07-05 18:47:01 +0200

    correct animations transition, start to set jump transition

--- 3D/Windows/RPGguy/Assets/scripts/CharacterMotor.cs
+++ 3D/Windows/RPGguy/Assets/scripts/CharacterMotor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class CharacterMotor : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class CharacterMotor : MonoBehaviour
     public string inputRight;
     public string inputRun;
 
+    public string inputJump;
+
     public Vector3 jumpspeed;
     CapsuleCollider playerCollider;
 
@@ -30,26 +33,41 @@ public class CharacterMotor : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(inputFront) && !Input.GetKey(inputRun))
+        if (Input.GetKey(inputFront) && !Input.GetKey(inputRun) && !Input.GetKey(inputJump))
         {
             transform.Translate(0, 0, walkSpeeed * Time.deltaTime);
             animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isJumping", false);
+        }
+        else if (Input.GetKey(inputFront) && Input.GetKey(inputRun))
+        {
+            transform.Translate(0, 0, runSpeed * Time.deltaTime);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isJumping", false);
+        }
+        else if (Input.GetKey(inputFront) && Input.GetKey(inputJump))
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isJumping", true);
         }
         else
         {
             animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+
         }
 
-        if (Input.GetKey(inputRun) && Input.GetKey(inputFront))
+        if (Input.GetKeyDown(inputJump))
         {
-            transform.Translate(0, 0, runSpeed * Time.deltaTime);
-            animator.SetBool("isRunning", true);
-        }
-        else
-        {
+            GetComponent<Rigidbody>().AddForce(jumpspeed, ForceMode.Impulse);
+            animator.SetBool("isWalking", false);
             animator.SetBool("isRunning", false);
+            animator.SetBool("isJumping", true);
         }
-        
+
         if (Input.GetKey(inputBack))
         {
             transform.Translate(0, 0, -(walkSpeeed / 2) * Time.deltaTime);
